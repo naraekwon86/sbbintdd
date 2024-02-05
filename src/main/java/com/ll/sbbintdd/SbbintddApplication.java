59b75661e01d7512ea4d9e6950cad9076116a762
naraekwon86 <wde0530@gmail.com>
2024-02-05 13:11:39 +0900

    start

---
+++ src/main/java/com/ll/sbbintdd/SbbintddApplication.java
@@ -0,0 +1,13 @@
+package com.ll.sbbintdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbbintddApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbbintddApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ll/sbbintdd/SbbintddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.sbbintdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbbintddApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
